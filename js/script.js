e6266187c4ca4b04eb377c1f81ed481d5877450e
Fernanda Mascheti <43377020+femascheti@users.noreply.github.com>
2024-01-25 15:39:03 -0300

    aula06
    gerando nomes aleatórios

--- js/script.js
+++ js/script.js
@@ -7,24 +7,10 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
-const botaoIniciar = document.querySelector(".iniciar-btn");
-const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
-
-botaoIniciar.addEventListener('click', iniciaJogo);
-
-function iniciaJogo() {
-    atual = 0;
-    historiaFinal = "";
-    telaInicial.style.display = 'none';
-    caixaPerguntas.classList.remove("mostrar");
-    caixaAlternativas.classList.remove("mostrar");
-    caixaResultado.classList.remove("mostrar");
-    mostraPergunta();
-}
 
 function mostraPergunta() {
     if (atual >= perguntas.length) {
@@ -49,12 +35,7 @@ function mostraAlternativas() {
 function respostaSelecionada(opcaoSelecionada) {
     const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
-    if (opcaoSelecionada.proxima !== undefined) {
-        atual = opcaoSelecionada.proxima;
-    } else {
-        mostraResultado();
-        return;
-    }
+    atual++;
     mostraPergunta();
 }
 
@@ -80,3 +61,4 @@ function substituiNome() {
 }
 
 substituiNome();
+mostraPergunta();
